4654f93d1933502c680b318ce41e29f649d57b21
Venkatesh7981 <vkuruva51@gmail.com>
2025-01-02 19:13:31 +0530

    36videos

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -1,14 +1,68 @@
 import React, { Component } from 'react';
-import Navbar from './Components/Navbar';
 import News from './Components/News';
+import Navbar from './Components/Navbar';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
 
 class App extends Component {
+  setProgress = (progress) => {
+    console.log(`Progress: ${progress}`);
+  };
+  pageSize=10;
+
   render() {
     return (
       <div>
-        <Navbar title="NewsMonkey" aboutText="About Us" />
-        <News />
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={<News setProgress={this.setProgress} key="general" pageSize={this.pageSize} country="us" category="general" />}
+            />
+            <Route
+              exact
+              path="/business"
+              element={<News setProgress={this.setProgress} key="business" pageSize={this.pageSize} country="us" category="business" />}
+            />
+            <Route
+              exact
+              path="/entertainment"
+              element={<News setProgress={this.setProgress} key="entertainment" pageSize={this.pageSize} country="us" category="entertainment" />}
+            />
+            <Route
+              exact
+              path="/general"
+              element={<News setProgress={this.setProgress} key="general" pageSize={this.pageSize} country="us" category="general" />}
+            />
+            <Route
+              exact
+              path="/health"
+              element={<News setProgress={this.setProgress} key="health" pageSize={this.pageSize} country="us" category="health" />}
+            />
+            <Route
+              exact
+              path="/science"
+              element={<News setProgress={this.setProgress} key="science" pageSize={this.pageSize} country="us" category="science" />}
+            />
+            <Route
+              exact
+              path="/sports"
+              element={<News setProgress={this.setProgress} key="sports" pageSize={this.pageSize} country="us" category="sports" />}
+            />
+            <Route
+              exact
+              path="/technology"
+              element={<News setProgress={this.setProgress} key="technology" pageSize={this.pageSize} country="us" category="technology" />}
+            />
+          </Routes>
+        </Router>
       </div>
     );
   }
